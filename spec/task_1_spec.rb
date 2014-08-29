e51ffebf052290363d1fb0a27915f67e2c836fae
elliotlewis8923 <elewis8923@gmail.com>
2014-08-29 16:03:32 +0100

    passed second test

--- spec/task_1_spec.rb
+++ spec/task_1_spec.rb
@@ -7,4 +7,11 @@ describe Array do
 		expect(array.do_something).to eq array.inject
 	end
 
+	it 'do_something should do something' do
+		array = Array.new
+		array = ['donkey', 'apple', 'lithuania']
+		array = array.do_something { |yo, bro| yo + bro }
+		expect(array).to eq "donkeyapplelithuania"
+	end
+
 end
