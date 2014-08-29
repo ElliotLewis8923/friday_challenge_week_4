4fe6a2d438924fb4db6347e1822960250272d0b2
elliotlewis8923 <elewis8923@gmail.com>
2014-08-29 16:41:53 +0100

    passed second test

--- lib/task_2.rb
+++ lib/task_2.rb
@@ -1,7 +1,12 @@
 class Takeaway
 
 	def dishes
-		dishes = Hash.new
+		@dishes = Hash.new
+		@dishes = { :Chips => 3.99, 
+							:Kangaroo => 2.14,
+							:Rhino => 4.20,
+							:Laksa => 1.00,
+							:Toast => 12.98}
 	end
 
 end
--- spec/task_2_spec.rb
+++ spec/task_2_spec.rb
@@ -2,9 +2,16 @@ require 'task_2'
 
 describe Takeaway do 
 
-	it 'should have a list of dishes' do
+	it 'should have a hash' do
 		takeaway = Takeaway.new
 		expect(takeaway.dishes.class).to eq Hash
 	end
-	
+
+	it 'the hash should have a list of dishes and corresponding prices' do
+		takeaway = Takeaway.new
+		expect(takeaway.dishes.length).to be > 0
+		expect(takeaway.dishes[:Chips].class).to eq Float
+
+	end
+
 end
